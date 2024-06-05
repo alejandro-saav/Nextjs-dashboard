e97f4d52e407c3cf830bcd950514a1c50fac6477
NexxRoxx <alejandroTPD@hotmail.com>
2024-06-05 13:56:40 -0500

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Customers() {
+  return <p>THIS IS THE CUSTOMERS PAGE</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices() {
+  return <p>THIS IS THE INVOICES PAGE</p>;
+}
